b714530e89e9aa467152c2dbbf0dea536abdabc5
miray <miray.mutlu@ozu.edu.tr>
2022-08-22 14:10:26 +0300

    useParams added for char_id

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import {useParams} from "react-router-dom"
 
 function Detail() {
+  const {char_id} = useParams();
+  console.log(char_id);
   return <div>Detail</div>;
 }
 
