5c09ed46eae09e135dbc227585fe35111abb293f
miray <miray.mutlu@ozu.edu.tr>
2022-08-22 15:00:14 +0300

    navbar added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home/index.jsx";
 import Detail from "./pages/Detail/index.jsx";
 
 function App() {
   return (
     <Router>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
       <div>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
