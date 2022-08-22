939732b1fea24351d6b47267ef024bd2f6a08ceb
miray <miray.mutlu@ozu.edu.tr>
2022-08-22 12:58:29 +0300

    detail page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/index.jsx";
+import Detail from "./pages/Detail/index.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/character/:character_id" element={<Detail />}></Route>
         </Routes>
       </div>
     </Router>
