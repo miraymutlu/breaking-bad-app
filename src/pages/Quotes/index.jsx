80e96d6b0f0e4ded4ad12bacb4bec95f98f45f9f
miray <miray.mutlu@ozu.edu.tr>
2022-08-22 15:21:57 +0300

    fetchQuotes created

--- src/pages/Quotes/index.jsx
+++ src/pages/Quotes/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import {useSelector} from "react-redux";
 
 function Quotes() {
+  const data = useSelector((state) => state.quotes.items);
   return <div>Quotes</div>;
 }
 
--- src/redux/quotesSlice.jsx
+++ src/redux/quotesSlice.jsx
@@ -1,10 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchQuotes = createAsyncThunk("quotes/getQuotes", async () => {
+  const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/quotes`);
+  return res.data;
+});
 
 export const quotesSlice = createSlice({
-    name:"quotes",
-    initialState: {},
-    reducers:{},
-    extraReducers:{}
+  name: "quotes",
+  initialState: {},
+  reducers: {
+    items: [],
+  },
+  extraReducers: {},
 });
 
 export default quotesSlice.reducer;
