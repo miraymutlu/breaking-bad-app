f6fc723f08ffcae42e3bc6c921837dd1dd22d4b2
miray <miray.mutlu@ozu.edu.tr>
2022-08-22 17:57:55 +0300

    styles.css created for Item.jsx

--- src/pages/Quotes/Item.jsx
+++ src/pages/Quotes/Item.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./styles.css"
 
 function Item({ item }) {
-  return <div>{item.quote}</div>;
+  return (
+    <div className="quote_item">
+      <q>{item.quote}</q>
+    </div>
+  );
 }
 
 export default Item;
