14dcda4894727741e6cdf0351618d14bedc7d7ba
miray <miray.mutlu@ozu.edu.tr>
2022-08-22 14:49:08 +0300

    loading added to details component

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,10 +1,31 @@
-import React from "react";
-import {useParams} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Loading from "../../components/Loading";
 
 function Detail() {
-  const {char_id} = useParams();
-  console.log(char_id);
-  return <div>Detail</div>;
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { char_id } = useParams();
+
+  useEffect(() => {
+    axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters/${char_id}`)
+      .then((res) => res.data)
+      .then((data) => setChar(data[0]))
+      .finally(() => setLoading(false));
+  }, [char_id]);
+
+  return (
+    <div>
+      {loading && <Loading />}
+      {char && (
+        <div>
+          <h1>{char.name}</h1>
+          <img src={char.img} alt={char.name} style={{ width: "50%" }} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
