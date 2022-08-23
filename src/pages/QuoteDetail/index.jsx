d03d7ed21bcb5a5f4c752b383ed4a34585f40cbb
miray <miray.mutlu@ozu.edu.tr>
2022-08-23 12:09:54 +0300

    finished

--- src/pages/QuoteDetail/index.jsx
+++ src/pages/QuoteDetail/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { quotesSelector } from "../../redux/quotesSlice";
 
 function QuoteDetail() {
+  const { quote_id } = useParams();
+  const items = useSelector(quotesSelector);
+  const quote = items.find((item)=> item.quote_id === Number(quote_id));
+
+  if(!quote){
+    return <Navigate to="/quotes" />;
+  }
+
   return (
-    <div>QuoteDetail</div>
-  )
+    <div>
+      <h1>Quote Detail</h1>
+      <pre>{JSON.stringify(quote,null,2)}</pre>
+    </div>
+  );
 }
 
-export default QuoteDetail
+export default QuoteDetail;
